b24a7b2c122cceaf9f28248ae1f9051430e959d9
eleonoravercesi <eleonora.vercesi@libero.it>
2025-11-11 18:03:20 +0100

    Polyhedral study

--- instances_template.py
+++ instances_template.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def pick_one_instance(instance_string):
     if instance_string == "jansen_land_maark_2018":
         M = [
@@ -25,10 +27,40 @@ def pick_one_instance(instance_string):
             [0,    0, 0, 0, 0, 0, 0, 1,  1,  1,  1,  1,  0],
             [0,    0, 0, 0, 0, 0, 0, 0,0, 0,  1,  1,   1],
         ]
+    elif instance_string == "koppany_2025":
+        # Block 1
+        M_1 = np.ones((3, 5))
+        M_2 = np.ones((4, 3))
+        M_3 = np.ones((2, 3))
+
+        n_machines = 9
+        n_jobs = 13
+
+        M = np.zeros((n_machines, n_jobs))
+
+        M[:3, :5] = M_1
+        M[[2, 3], 5] = [2, 2]
+        M[3:7, [6, 7, 8]] = M_2
+        M[[6, 7], 9] = [2, 2]
+        M[7:9, 10:13] = M_3
+
+        M = M.tolist()
+    elif instance_string == "koppany_2025_short":
+        M = [
+            [   1,  1,  1,  0,  0,  0,  0,  0,  0],
+            [   1,  1,  1,  2,  0,  0,  0,  0,  0],
+            [   0,  0,  0,  2,  1,  0,  0,  0,  0],
+            [   0,  0,  0,  0,  1,  0,  0,  0,  0],
+            [   0,  0,  0,  0,  1,  2,  0,  0,  0],
+            [   0,  0,  0,  0,  0,  2,  1,  1,  1],
+            [   0,  0,  0,  0,  0,  0,  1,  1,  1],
+        ]
+
     else:
         raise ValueError(f"Instance '{instance_string}' not recognized, must be one of\n" + \
                          f"jansen_land_maark_2018,\n" + f"jansen_land_maark_2018_modified,\n" + \
-                            f"jansen_land_maark_2018_expanded\n"
+                            f"jansen_land_maark_2018_expanded\n" + "koppany_2025\n" + \
+                         "koppany_2025_short"
                        )
 
     n = len(M[0])
--- vertices_generation.py
+++ vertices_generation.py
@@ -1,13 +1,20 @@
+import numpy as np
+
 from instance import InstanceRestrictedAssignment
 from instances_template import pick_one_instance
 import subprocess
 
 if __name__ == "__main__":
-    n_jobs, n_machines, M = pick_one_instance("jansen_land_maark_2018")
     filename = "model.lp"
+
+    input_matrix = "koppany_2025_short"
+
+    n_jobs, n_machines, M = pick_one_instance(input_matrix)
+
     instance = InstanceRestrictedAssignment(n_jobs, n_machines, generate=False, M = M)
     T = instance.opt_LP(verbose=False)
-    instance.get_A_b(T, filename=filename)
+    instance.get_A_b_eq(T, filename=filename)
+    #T_IP = instance.opt_IP(verbose=False)
 
     # Write polimake script
     """
@@ -23,4 +30,4 @@ if __name__ == "__main__":
     F.write("print $p -> VERTICES;\n")
     F.close()
 
-    subprocess.run("polymake --script to_run.txt", shell=True)
+    subprocess.run("polymake --script to_run.txt > vertices_eq.txt", shell=True)

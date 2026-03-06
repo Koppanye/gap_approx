2bd73a33d70150fc881d2c9abae2188e79a569ee
eleonoravercesi <eleonora.vercesi@libero.it>
2026-03-06 11:03:57 +0100

    Adding new files for drafts

--- drafts/draft_dual_on_instance.py
+++ drafts/draft_dual_on_instance.py
@@ -12,22 +12,34 @@ from pyscipopt import Model, SCIP_RESULT
 # y_non_zero = {(0, (0, 1, 2, 8))  :  5/21, (0, (0, 1, 5, 9))  :  1/3, (0, (0, 2, 3, 4, 6))  :  4/21, (0, (1, 2, 3, 6, 7, 9))  :  1/7, (0, (2, 5, 6, 7, 8, 9))  :  2/21, (1, (1, 2, 4, 9))  :  5/21, (1, (0, 2, 3, 5, 7))  :  2/21, (1, (0, 4, 7, 8, 9))  :  2/21, (1, (0, 3, 6, 7, 8, 9))  :  1/21, (1, (1, 3, 6, 7, 8, 9))  :  1/21, (1, (3, 4, 5, 6, 7, 8))  :  10/21}
 
 
-# # Instance 2
-# p = [30, 46, 81, 30, 55, 71, 33, 22, 5, 93]
-# T = 233
+# Instance 2
+p = [30, 46, 81, 30, 55, 71, 33, 22, 5, 93]
+T = 233
+y_non_zero = {
+    (0, (0, 4, 6, 7, 9)) : 1/2, (0, (1, 2, 3, 5, 8)) : 1/2, (1, (0, 1, 2, 5, 8)) : 1/2, (1, (3, 4, 6, 7, 9)) : 1/2
+}
 
+# # Instance 3
+# p = [23, 48, 20, 9, 19, 21, 21, 46, 17, 43, 42, 17, 3]
+# T = 165
+# y_non_zero = {(0, (1, 3, 6, 9, 10)) : 1/4, (0, (1, 3, 7, 9, 11)) : 1/28, (0, (0, 4, 5, 9, 10, 11)) : 1/28, (0, (1, 3, 7, 8, 10, 12)) : 1/4, (0, (2, 4, 5, 7, 8, 10)) : 1/14, (0, (2, 4, 6, 7, 10, 11)) : 1/28, (0, (1, 3, 5, 6, 7, 11, 12)) : 1/28, (0, (2, 4, 5, 9, 10, 11, 12)) : 5/28, (0, (0, 2, 5, 6, 8, 9, 11, 12)) : 3/28, (1, (0, 1, 2, 3, 4, 7)) : 3/7, (1, (5, 6, 7, 8, 9, 11)) : 1/7, (1, (0, 2, 5, 6, 8, 10, 11, 12)) : 5/28, (1, (0, 4, 5, 6, 8, 9, 11, 12)) : 1/4}
+#
 
-# Instance 3
-p = [23, 48, 20, 9, 19, 21, 21, 46, 17, 43, 42, 17, 3]
-T = 165
-y_non_zero = {(0, (1, 3, 6, 9, 10)) : 1/4, (0, (1, 3, 7, 9, 11)) : 1/28, (0, (0, 4, 5, 9, 10, 11)) : 1/28, (0, (1, 3, 7, 8, 10, 12)) : 1/4, (0, (2, 4, 5, 7, 8, 10)) : 1/14, (0, (2, 4, 6, 7, 10, 11)) : 1/28, (0, (1, 3, 5, 6, 7, 11, 12)) : 1/28, (0, (2, 4, 5, 9, 10, 11, 12)) : 5/28, (0, (0, 2, 5, 6, 8, 9, 11, 12)) : 3/28, (1, (0, 1, 2, 3, 4, 7)) : 3/7, (1, (5, 6, 7, 8, 9, 11)) : 1/7, (1, (0, 2, 5, 6, 8, 10, 11, 12)) : 5/28, (1, (0, 4, 5, 6, 8, 9, 11, 12)) : 1/4}
-
-
-# # Instance 4
+# # Instance 4 OK with both methods
 # p = [12, 59, 56, 8, 16, 25]
 # T = 89
 # ass = [1, 1, 0, 0, 1, 0]
+# y_non_zero = {
+#     (0, (2, 3, 5)) : 1, (1, (0, 1, 4)) : 1,
+# }
 
+# # Instance 5, infeas with option 2, suboptimal with option 1
+# p = [2, 2, 2, 1]
+# T = 4
+# y_non_zero = {
+# (0, (0, 3)) : 1/2, (0, (1, 2)) : 1/2, (1, (0, 1)) : 1/2, (1, (2, 3)) : 1/2,
+# }
+# ass = [1, 0, 1, 0]
 
 # For everything
 n_jobs = len(p)
@@ -42,14 +54,14 @@ M = np.asarray([p for _ in range(n_machines)])
 
 
 
-# -----------------------
+# #-----------------------
 # # If you need to solve the IP
 # print(f"p = {p}", flush=True)
 # instance = InstanceRestrictedAssignment(n_jobs, n_machines, generate=False, M = M)
 # print("Instance generated!", flush=True)
 # ass, T_int = instance.opt_IP(verbose=True)
-
-# -----------------------
+#
+# #-----------------------
 # # # If you need to solve the LP
 # instance = InstanceRestrictedAssignment(n_jobs, n_machines, generate=False, M = M)
 # print("Instance generated!", flush=True)
@@ -57,8 +69,8 @@ M = np.asarray([p for _ in range(n_machines)])
 # for k in y.keys():
 #     if y[k] > TOL:
 #         print(k, ":", Fraction(y[k]).limit_denominator(), end=", ")
-
-print(f"Optimal makespan {T}")
+#
+# print(f"Optimal makespan {T}")
 
 
 # Determining valid configurations (with makespan at most T) for each machine in the form of a dict
@@ -69,7 +81,7 @@ configs = {i: [c for length in range(1, n_jobs + 1) for c in combinations([j for
 #j_pairs = combinations(range(n_jobs), 2)
 #for j1, j2 in j_pairs:
 j1 = 0
-j2 = 3
+j2 = 1
 
 # Nice, now write the dual
 model = Model('Restricted assignment with 2 processing times')
@@ -107,8 +119,11 @@ for i in range(n_machines):
 # Add a fake constraint
 # model.addCons(-sum(u) + sum(v) >= 2*TOL, name="absurdum")
 
+
 # Try e feasible solution
 u_feas = 1
+
+# Option 1
 v_feas = []
 for j in range(n_jobs):
     configs_j = [C for C in configs[0] if j in C]
@@ -117,12 +132,49 @@ for j in range(n_jobs):
     max_weight_config_j = max(weight_config_j)
     v_feas.append(Fraction(u_feas * p[j] / max_weight_config_j).limit_denominator())
 
+# Option 2
+# # Forcing solutions
+# v_feas = []
+# for j in range(n_jobs):
+#     v_j_feas_den = 0
+#     for i, C in y_non_zero:
+#         if j in C:
+#             v_j_feas_den += y_non_zero[(i, C)] * sum(p[q] for q in C) * u_feas
+#     v_feas.append(Fraction(p[j] / v_j_feas_den).limit_denominator())
+#
+# # Add fake constraint to force the solution to be as I wish
+# model.addCons(u[0] == u_feas, name="force_u")
+# model.addCons(u[1] == u_feas, name="force_u_2")
+#
+# for j in range(n_jobs):
+#     model.addCons(v[j] == v_feas[j], name=f"force_v_{j}")
+# # -------------------------
+
+# # Option 3
+# # Forcing solutions
+# v_feas = []
+# for j in range(n_jobs):
+#     v_j_feas = 0
+#     for i, C in y_non_zero:
+#         if j in C and j1 in C and j2 in C:
+#             v_j_feas += y_non_zero[(i, C)] * u_feas / max([len(C) for C in y_non_zero])
+#
+#     v_feas.append(v_j_feas)
+
 # Add fake constraint to force the solution to be as I wish
 model.addCons(u[0] == u_feas, name="force_u")
 model.addCons(u[1] == u_feas, name="force_u_2")
 
 for j in range(n_jobs):
     model.addCons(v[j] == v_feas[j], name=f"force_v_{j}")
+# -------------------------
+
+
+# # Just force u
+# u_feas = 1
+# model.addCons(u[0] == u_feas, name="force_u")
+# model.addCons(u[1] == u_feas, name="force_u_2")
+# #----------------------------
 
 model.optimize()
 
--- utils.py
+++ utils.py
@@ -1,1 +1,54 @@
 TOL = 1e-6
+from pyscipopt import Model, quicksum
+
+def find_covering(n, C_list):
+    """
+    When m = 3, you can cover [n] with three disjoint sets. This function finds those three sets.
+
+    Parameters
+    ----------
+    n : int
+        The number of elements in the universe.
+    C_list : list of lists
+        The list of configurations (subsets of [n]).
+
+    Returns
+    -------
+    list of lists
+        A list of three lists, each containing the indices of the elements in the corresponding set.
+    """
+    model = Model("three_disjoint_cover")
+
+    m = len(C_list)
+
+    # decision variables
+    x = {}
+    for j in range(m):
+        x[j] = model.addVar(vtype="B", name=f"x_{j}")
+
+    # exactly three sets
+    model.addCons(quicksum(x[j] for j in range(m)) == 3)
+
+    # coverage constraints
+    for i in range(n):
+        model.addCons(
+            quicksum(x[j] for j in range(m) if i in C_list[j]) >= 1
+        )
+
+    # disjointness constraints
+    for i in range(m):
+        for j in range(i + 1, m):
+            # If the intersections of C_list[i] and C_list[j] is not empty, then they cannot both be selected
+            if set(C_list[i]).intersection(set(C_list[j])):
+                model.addCons(x[i] + x[j] <= 1)
+
+    # dummy objective (feasibility problem)
+    model.setObjective(0)
+
+    model.optimize()
+
+    if model.getStatus() == "optimal":
+        sol = [j for j in range(m) if model.getVal(x[j]) > 0.5]
+        return sol
+    else:
+        return None
